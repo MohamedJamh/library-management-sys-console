db4e72de2fd320c74315d102ad672c62ca5e18ec
MohamedJamh <jam7.mohamed@gmail.com>
2023-09-06 11:10:35 +0200

    Testing conneciton to database with jdbc

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,9 +6,39 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     private static Scanner myScanner = new Scanner(System.in);
+    private static Connection connection;
 
     public static void main(String[] args) {
-        testObjects();
+        connectToMysql();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from users");
+            while (resultSet.next()) {
+                System.out.println("cin : " + resultSet.getString("cin")
+                        + " name : " + resultSet.getString("first_name")
+                        + resultSet.getString("last_name")
+                );
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void connectToMysql(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/ycminisas",
+                    "root", "");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void disconnect(){
+        connection.close();
     }
     public static void testObjects(){
 
@@ -16,12 +47,12 @@ public class Main {
 
 
         System.out.println("------users------");
-        System.out.println(librarian.toString());
-        System.out.println(reader.toString());
+        System.out.println(librarian);
+        System.out.println(reader);
         System.out.println("------book------");
 
         Book book1 = new Book("XCVSDF333","Harry Potter",4,4,0,0, 100.00F,"J.K Rolling");
-        System.out.println(book1.toString());
+        System.out.println(book1);
         System.out.println("------copies------");
 
         ArrayList<Copy> copies = new ArrayList<>();
@@ -31,29 +62,21 @@ public class Main {
             System.out.println(copy);
         }
         System.out.println("------ reservation ------");
-
         System.out.println("Enter the copy number : ");
         int copyId = Integer.parseInt(myScanner.nextLine());
-
-        Reservation res = new Reservation(copies.get(copyId - 1).getId(),1,"12/12/2020");
-        System.out.println(res.toString());
+        Reservation res = new Reservation(1,copies.get(copyId - 1).getId(),"12/12/2020");
+        System.out.println(res);
         reader.setReservations(res);
         copies.get(copyId - 1).setReservedByReaderCin(reader.getCin());
-
-
 
         System.out.println("------ reserved copy ------");
         copies.get(copyId - 1).isBorrowed();
         System.out.println(copies.get(copyId - 1));
         System.out.println("------book collection after reservation------");
         book1.setBorrowed(1);
-        System.out.println(book1.toString());
+        System.out.println(book1);
         System.out.println("------reader after reservation------");
-        System.out.println(reader.toString());
-
-
-
-
+        System.out.println(reader);
     }
 }
 
