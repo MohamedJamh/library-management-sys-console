21d7685da61d78558970595745b1d44509a11bcd
MohamedJamh <jam7.mohamed@gmail.com>
2023-09-13 20:49:32 +0100

    Throwing exception login improved Reservation book done and returning book, getting all borrowed books feature added, deleting book logic implemented Statistic method added

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     private _Db mysql;
     public static void main(String[] args) {
+
         _Db mysql = new _Db();
         AuthenticaionController.login();
     }
